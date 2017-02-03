a618cab9dcea77a7a02a50086de87d0b48408a1f
Younes Abdel <jaymz1979@gmail.com>
2017-02-03 17:09:58 +0000

    nothing

--- SpringStarter/src/main/java/com/example/SpringStarterApplication.java
+++ SpringStarter/src/main/java/com/example/SpringStarterApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringStarterApplication {
 	
 
-	public static void main(String[] args) {System.out.println("youyou");
+	public static void main(String[] args) {
 		SpringApplication.run(SpringStarterApplication.class, args);
 	}
 	
