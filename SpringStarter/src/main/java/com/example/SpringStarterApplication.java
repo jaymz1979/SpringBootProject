529ff6f1446b36f0e95bc276a884d0b76c8fe543
Younes Abdel <jaymz1979@gmail.com>
2017-02-03 16:53:25 +0000

    sysout

--- SpringStarter/src/main/java/com/example/SpringStarterApplication.java
+++ SpringStarter/src/main/java/com/example/SpringStarterApplication.java
@@ -1,18 +1,13 @@
 package com.example;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.example.entity.Place;
-import com.example.repository.PlaceRepository;
 
 @SpringBootApplication
 public class SpringStarterApplication {
 	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {System.out.println();
 		SpringApplication.run(SpringStarterApplication.class, args);
 	}
 	
